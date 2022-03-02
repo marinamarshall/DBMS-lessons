812d2b24a8f1db7b2efe8865e76a8b7218b77b00
marinamarshall <marinamarshall2@gmail.com>
2022-03-02 20:10:52 +0000

    Add to py file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -4,16 +4,19 @@ import psycopg2
 # Connect to "chinook" database
 connection = psycopg2.connect(database="chinook")
 
-# build a cursor object of the database
+# build a cursor object of the database, same as a set/list (array in JS)
 cursor = connection.cursor()
 
-# fetch the results (multiple)
+# Query 1 - select all records from the "Artist" table
+cursor.execute('SELECT * FROM "ARTIST"')
+
+# fetch the results (multiple records from database)
 results = cursor.fetchall()
 
 # fetch the result (single)
 # results = cursor.fetchone()
 
-# close the connection
+# once results have been fetched, close the connection to the database
 connection.close()
 
 # print results
